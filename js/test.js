764c07f65224ce06a1dd44e43c97c227fa0724fa
Mathieu Margollé <mathieu.margolle@yandex.com>
2018-03-07 13:51:36 +0100

    Tests avec mapbox, couleurs sur régions, limitation de la zone

--- js/test.js
+++ js/test.js
@@ -25,29 +25,60 @@ map.on('load', function () {
 		.then(function(data) {
 
 	  map.addSource('regions', {
-	      "type": "geojson",
-	      "data": data
+      "type": "geojson",
+      "data": data
 	  });
+
+	  var style_regions = {
+      "fill-opacity": 0.9,
+      "fill-outline-color": "rgba(0,0,0,0.2)",
+      "fill-color": [
+        'interpolate',
+        ['linear'],
+        ['to-number', ['get', 'INSEE_REG']],
+        0,  '#fef0d9',
+				25, '#fdcc8a',
+				50, '#fc8d59',
+				75, '#e34a33',
+				100,'#b30000'
+      ],
+    };
+    var style_regions_hover = Object.assign({}, style_regions);
+    style_regions_hover["fill-opacity"] = 1;
+    style_regions_hover["fill-outline-color"] = 'black';
 
 	  map.addLayer({
 	    "id": "regions",
 	    "type": "fill",
 	    "source": "regions",
-	    "paint": {
-	        "fill-outline-color": "rgba(0,0,0,1)",
-	        "fill-color": "rgba(255,0,0,0.5)"
-	    }
+	    "paint": style_regions
+		}, 'place-city-sm');
+
+	  map.addLayer({
+	    "id": "regions-hover",
+	    "type": "fill",
+	    "source": "regions",
+	    "paint": style_regions_hover,
+      "filter": ["==", "INSEE_REG", ""]
 		}, 'place-city-sm');
 
 		var bounds = turf.bbox(data);
 		map.fitBounds(bounds, {padding: 20, animate:false});
-
-		map.on('render', toto);
-		function toto(e) {
+		map.on('render', setMaxBoundsOnce);
+		function setMaxBoundsOnce(e) {
 			var nbounds = map.getBounds();
 			map.setMaxBounds(nbounds);
-			map.off('render', toto);
+			map.off('render', setMaxBoundsOnce);
 		};
+
+    map.on("mousemove", "regions", function(e) {
+    	map.getCanvas().style.cursor = 'pointer';
+      map.setFilter("regions-hover", ["==", "INSEE_REG", e.features[0].properties.INSEE_REG]);
+    });
+    map.on("mouseleave", "regions", function(e) {
+    	map.getCanvas().style.cursor = '';
+      map.setFilter("regions-hover", ["==", "INSEE_REG", ""]);
+    });
 
 	});
 
